97d3ad85eb41f50b2122f2fbd09cf7c2df08ea9f
Katharine Jiang <k.jiang@berkeley.edu>
2020-07-09 01:03:41 -0700

    scaffolding for join us page

--- script/index.js
+++ script/index.js
@@ -5,3 +5,29 @@ function openNav() {
 function closeNav() {
   document.getElementById("myNav").style.height = "0%";
 }
+
+/* Join Us Page */
+var joinVCIsOpen = false
+var joinFounderIsOpen = false
+
+function toggleJoinVC() {
+  if (joinVCIsOpen) {
+    // document.getElementById("join-vc").style.display="none";
+    document.getElementById("join-vc").classList.remove("join-answer-active");
+  } else {
+    // document.getElementById("join-vc").style.display="block";
+    document.getElementById("join-vc").classList.add("join-answer-active");
+  }
+  joinVCIsOpen = !joinVCIsOpen;
+}
+
+function toggleJoinFounder() {
+  if (joinFounderIsOpen) {
+    // document.getElementById("join-vc").style.display="none";
+    document.getElementById("join-founder").classList.remove("join-answer-active");
+  } else {
+    // document.getElementById("join-vc").style.display="block";
+    document.getElementById("join-founder").classList.add("join-answer-active");
+  }
+  joinFounderIsOpen = !joinFounderIsOpen;
+}
